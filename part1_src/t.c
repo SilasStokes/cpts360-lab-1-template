2447f3cb23b52adfc9dbf4ad624ec0cde2a70d11
silas <stokessilas@gmail.com>
2022-01-15 13:25:21 -0800

    updated lab 1

---
+++ part1_src/t.c
@@ -0,0 +1,8 @@
+
+
+
+// please put your main and code in this file. All contained.
+// Use the provided mk script to compile your code. You are welcome to change the mk script if you'd like
+// but know that is how we will be batch compiling code using mk on the back end so your code will need to be able
+// to run when that bash script is ran.
+// Most code can be grabbed from the .html file in the root.
